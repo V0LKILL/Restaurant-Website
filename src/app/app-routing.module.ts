3a69a33ea9f00616e2d80a4d08791a9d67e8889c
Mike <volk55project@gmail.com>
2022-10-02 17:08:30 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {HomeComponent} from "./pages/home/home.component";
+import {AboutComponent} from "./pages/about/about.component";
+import {MenuComponent} from "./pages/menu/menu.component";
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'menu', component: MenuComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/http/menu/menu.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+import {Observable} from "rxjs";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MenuService {
+
+  constructor(private http: HttpClient) {
+
+    public get(): Observable<any> {
+
+        return this.http.get(`${environment.url}/assets/json/menu.json`)
+    }
+
+  }
+}
